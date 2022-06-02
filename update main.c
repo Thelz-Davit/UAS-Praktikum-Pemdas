955ed177c5e58268c7a9867c31eab683abd2fa0a
berka <105926323+lonnaone@users.noreply.github.com>
2022-06-02 08:40:42 +0700

    Create update main.c

---
+++ update main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "roti.c"
+
+int main() {
+    int pilih;
+    int cek = CheckUser();
+    if (cek == 1)
+    {
+        do
+        {
+            printf("========================================\n");
+            printf("|  Menu Pilihan Toko Roti Cake Bakery  |\n");
+            printf("========================================\n");
+            printf("|  1. Create                           |\n");
+            printf("|  2. Search                           |\n");
+            printf("|  3. Update                           |\n");
+            printf("|  4. Delete                           |\n");
+            printf("========================================\n");
+            printf("Masukkan Pilihan : ");
+            scanf("%d", &pilih);
+
+            switch(pilih){
+                case 1:
+                    Create();
+                    break;
+                case 2:
+                    Search();
+                    break;
+                case 3:
+                    Update();
+                    break;
+                case 4:
+                    Delete();
+                    break;
+                default:
+                    printf("pilihan tidak ada\n");
+                    break;
+            }
+            printf("lanjut? ");
+            scanf("%s", &lanjut);
+        } while (lanjut=='y');
+    }
+
+
+
+
+    // userlogin();
+    // CreateRoti();
+    return 0;
+}
